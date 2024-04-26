77563b8c980043e0815550c7ad13038d7862258c
vjsujal <www.sujalvj121@gmail.com>
2024-04-26 21:12:35 +0530

    Added Home Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Home from './components/home.jsx';
+import Footer from './components/footer.jsx';
 import Transcript from './components/transcript.jsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'tailwindcss/tailwind.css';
 
 
 const App = () => {
@@ -14,6 +16,7 @@ const App = () => {
     <>
       <RouterProvider router={router} />
       <ToastContainer />
+      <Footer />
     </>
   );
 }
--- Frontend/src/components/home.jsx
+++ Frontend/src/components/home.jsx
@@ -5,16 +5,107 @@ import UploadBox from './uploadbox';
 
 const Home = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
-    const handleDialogOpen = () => {
-        setIsOpen(true);
-    };
-    return (
-        <div className="container">
+  const [isOpen, setIsOpen] = useState(false);
+  const handleDialogOpen = () => {
+    setIsOpen(true);
+  };
+  return (
+    <div className="body">
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center" bis_skin_checked="1">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">First Link</a>
+            <a className="mr-5 hover:text-gray-900">Second Link</a>
+            <a className="mr-5 hover:text-gray-900">Third Link</a>
+            <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          </nav>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-12 py-28 mx-auto flex items-center justify-center">
+          <div className="lg:w-2/5 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+            <h1 className="text-gray-900 text-5xl title-font font-bold mb-4">Audio to Text</h1>
+            <p className="leading-relaxed mb-4 my-8">Transcribe audio to text automatically, using AI. Over +120 languages supported.</p>
+            <UploadBox isOpen={isOpen} onClose={() => setIsOpen(false)} />
+            <button onClick={handleDialogOpen} className="flex text-white items-center justify-center bg-blue-500 border-0 py-4 px-12 focus:outline-none hover:bg-blue-600 rounded-full">Transcribe your audio to text <div className="shadow-neutral-1000/25 -mr-[15px] grid items-center justify-center rounded-full p-3 mx-3 shadow-xl bg-white text-blue-600  rtl:-scale-x-100" bis_skin_checked="1"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-[18px] w-[18px]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="16 16 12 12 8 16"></polyline><line x1="12" y1="12" x2="12" y2="21"></line><path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path><polyline points="16 16 12 12 8 16"></polyline></svg></div></button>
+          </div>
+          <img alt="ecommerce" className="lg:w-3/5 w-full lg:h-auto h-64 object-cover object-center rounded" style={{ objectFit: 'contain' }} src="https://cdn-site-assets.veed.io/cdn-cgi/image/width=1280,quality=75,format=auto/Audio_to_Text_26b67a012c/Audio_to_Text_26b67a012c.png" />
+        </div>
+
+
+        {/* <div className="container">
             <UploadBox isOpen={isOpen} onClose={() => setIsOpen(false)} />
             <button onClick={handleDialogOpen}>Upload File</button>
+        </div> */}
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto" bis_skin_checked="1">
+          <div className="flex flex-col text-center w-full mb-16" bis_skin_checked="1">
+            <h1 className="text-gray-900 text-4xl title-font font-medium mb-4">How to transcribe audio to text:</h1>
+          </div>
+          <div className="flex flex-wrap -m-4" bis_skin_checked="1">
+            <div className="p-4 lg:w-1/3 md:w-1/2" bis_skin_checked="1">
+              <div className="h-full flex flex-col items-center text-center" bis_skin_checked="1">
+                <img alt="team" className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://cdn-site-assets.veed.io/cdn-cgi/image/width=640,quality=75,format=auto/Upload_or_record_d9784e01b0/Upload_or_record_d9784e01b0.png" />
+                <div className="w-full" bis_skin_checked="1">
+                  <h2 className="title-font font-bold text-2xl text-gray-900">Upload or record</h2>
+                  <p className="mb-4">Upload your audio or video.</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2" bis_skin_checked="1">
+              <div className="h-full flex flex-col items-center text-center" bis_skin_checked="1">
+                <img alt="team" className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://cdn-site-assets.veed.io/cdn-cgi/image/width=640,quality=75,format=auto/Auto_transcribe_and_translate_f9f7a99235/Auto_transcribe_and_translate_f9f7a99235.png" />
+                <div className="w-full" bis_skin_checked="1">
+                  <h2 className="title-font font-bold text-2xl text-gray-900">Auto-transcribe and translate</h2>
+                  <p className="mb-4">Auto-transcribe your video from the Subtitles menu. You can also translate your transcript to over 120 languages. Select a language and translate the transcript instantly.</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2" bis_skin_checked="1">
+              <div className="h-full flex flex-col items-center text-center" bis_skin_checked="1">
+                <img alt="team" className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://cdn-site-assets.veed.io/cdn-cgi/image/width=640,quality=75,format=auto/Review_and_export_39508d3634/Review_and_export_39508d3634.png" />
+                <div className="w-full" bis_skin_checked="1">
+                  <h2 className="title-font font-bold text-2xl text-gray-900">Review and export</h2>
+                  <p className="mb-4">Review and edit the transcription if necessary. Just click on a line of text and start typing. Download your transcript in VTT, SRT, or TXT format.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
-    );
+      </section>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-12 py-28 mx-auto flex items-center justify-center">
+          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+            <h1 className="text-gray-900 text-2xl title-font font-bold mb-4">Instant transcription downloads for better documentation</h1>
+            <p className="leading-relaxed mb-4 my-8">VEED uses cutting-edge technology to transcribe your audio to text at lightning-fast speed. Download your transcript in one click and keep track of your records better—without paying for expensive transcription services. Get a written copy of your recordings instantly and one proofread for 100% accuracy. Downloading transcriptions is available to premium subscribers. Check our pricing page for more info.</p>
+          </div>
+          <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" style={{ objectFit: 'contain' }} src="https://cdn-site-assets.veed.io/cdn-cgi/image/width=1024,quality=75,format=auto/Instant_transcription_downloads_for_better_documentation_ddcb6e11a1/Instant_transcription_downloads_for_better_documentation_ddcb6e11a1.png" />
+        </div>
+      </section>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-12 py-28 mx-auto flex items-center justify-center">
+          <img alt="ecommerce" className="lg:w-1/2 lg:pr-10 w-full lg:h-auto h-64 object-cover object-center rounded " style={{ objectFit: 'contain' }} src="https://cdn-site-assets.veed.io/cdn-cgi/image/width=1024,quality=75,format=auto/Convert_audio_to_text_and_create_globally_accessible_content_8373047d0a/Convert_audio_to_text_and_create_globally_accessible_content_8373047d0a.png" />
+          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+            <h1 className="text-gray-900 text-2xl title-font font-bold mb-4">Instant transcription downloads for better documentation</h1>
+            <p className="leading-relaxed mb-4 my-8">VEED uses cutting-edge technology to transcribe your audio to text at lightning-fast speed. Download your transcript in one click and keep track of your records better—without paying for expensive transcription services. Get a written copy of your recordings instantly and one proofread for 100% accuracy. Downloading transcriptions is available to premium subscribers. Check our pricing page for more info.</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
